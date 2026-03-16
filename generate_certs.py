c586afa700dfbedd8f406076303f60b4b46a5c27
Paulo Maia <pjm2098@email.vccs.edu>
2026-03-16 16:55:44 -0400

    Implemented the Server Certificate and Subject Alternative Names (SANs)

--- generate_certs.py
+++ generate_certs.py
@@ -38,3 +38,46 @@ def generate_ca_certificate():
         )
         .sign(ca_key, hashes.SHA256())
     )
+
+def generate_server_certificate(ca_key, ca_cert):
+    """Generate the server certificate signed by the CA"""
+
+    # The server gets its OWN key pair (separate from CA)
+    server_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+
+        # Server's identity
+    server_name = x509.Name([
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, "Grand Marina Hotel"),
+        x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, "MQTT Broker"),
+        x509.NameAttribute(NameOID.COMMON_NAME, "localhost"),
+    ])
+
+    server_cert = (
+        x509.CertificateBuilder()
+        .subject_name(server_name)
+        .issuer_name(ca_cert.subject)  # CA is the issuer!
+        .public_key(server_key.public_key())
+        .serial_number(x509.random_serial_number())
+        .not_valid_before(datetime.now(timezone.utc))
+        .not_valid_after(datetime.now(timezone.utc) + timedelta(days=365))
+        .add_extension(
+            x509.SubjectAlternativeName([
+                x509.DNSName("localhost"),
+                x509.IPAddress(ipaddress.IPv4Address("127.0.0.1")),
+            ]),
+            critical=False,
+        )
+        .add_extension(
+            x509.BasicConstraints(ca=False, path_length=None),
+            critical=True,
+        )
+        .sign(ca_key, hashes.SHA256())  # Signed by CA's key!
+    )
+
+    x509.SubjectAlternativeName([
+    x509.DNSName("localhost"),
+    x509.IPAddress(ipaddress.IPv4Address("127.0.0.1")),
+])
