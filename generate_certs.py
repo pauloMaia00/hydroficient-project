6b9ad282edba3069cc07f19a0bd7011ca205cf3e
Paulo Maia <pjm2098@email.vccs.edu>
2026-03-16 16:46:51 -0400

    Created file called generate_certs.py and implemented all of the imports needed

---
+++ generate_certs.py
@@ -0,0 +1,9 @@
+import ipaddress 
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
+
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
